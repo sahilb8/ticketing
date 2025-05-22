c8006ce81f9b583ea38566d28f57fd4540b56705
sahil.b <sahil.b@aavishk-tech.com>
2025-05-22 16:58:09 +0530

    added a few dev dependencies and moved creation of app to a seperate file

---
+++ auth/src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+app.use(errorHandler);
+
+export { app };
