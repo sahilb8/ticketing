7d3229ce9777e6a1d8dd5a96c3399fc3769bb87b
sahil.b <sahil.b@aavishk-tech.com>
2025-05-30 20:36:27 +0530

    moved errors and middleware to common

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -6,7 +6,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
+import { errorHandler } from '@sb7184ticketing/common';
 
 const app = express();
 app.set('trust proxy', true);
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { currentUser } from '../middleware/current-user';
+import { currentUser } from '@sb7184ticketing/common';
 
 const router = express.Router();
 
